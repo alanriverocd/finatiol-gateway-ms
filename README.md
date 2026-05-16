8f51c29e05c53165cc9b04deefefcb89d1c0d8c8
AlanIng <alan.riverocd@gmail.com>
2026-05-15 23:52:43 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+﻿# finatiol-gateway-ms
---
+++ src/main/java/com/finatiol/gateway/FinatiolGatewayMsApplication.java
@@ -0,0 +1,13 @@
+package com.finatiol.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinatiolGatewayMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinatiolGatewayMsApplication.class, args);
+	}
+
+}
